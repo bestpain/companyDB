541bda26d09bbdb43bcdd5c4a1035647844576d8
nitish ranjan <36075994+bestpain@users.noreply.github.com>
2020-09-27 23:50:52 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,11 +8,10 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./redux/reducers";
 import createSagaMiddleware from "redux-saga";
 const sagaMiddleware = createSagaMiddleware();
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
+  applyMiddleware(sagaMiddleware)
 );
 
 sagaMiddleware.run(rootSaga);
