c45a4ab8a3a3c32a7f4be59858fcc54dfbdba911
nitish ranjan <36075994+bestpain@users.noreply.github.com>
2020-09-28 17:30:35 +0530

    Update index.js
    added method to insert sample database data

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const userRouter = require("./router/user");
 const authRouter = require("./router/auth");
 const companyRouter = require("./router/company");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 //setup middlewares
 //Enable CORS for the App
@@ -18,5 +18,9 @@ app.use(express.json());
 app.use(userRouter);
 app.use(authRouter);
 app.use(companyRouter);
+
+//run this method to insert sample company data in database.
+// const addCompany = require("./addCompanyScript");
+// addCompany();
 
 app.listen(port, () => console.log(`server listening on port:${port}`));
