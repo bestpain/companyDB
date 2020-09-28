feb003ba2c887de1a806f4eb8c108138910b5e00
nitish ranjan <36075994+bestpain@users.noreply.github.com>
2020-09-28 08:57:30 +0530

    Update index.js
    mongo db uri

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
